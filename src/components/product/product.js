f00233f41108958a1c07ac29f4731734eafbdd1e
Wojciech <wojciechonoszko@gmail.com>
2022-05-17 22:00:32 +0200

    Update product.js

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -12,7 +12,39 @@ export const ProductList = (props) => {
   )
 }
 
+export const Profile = ({username, tag, location, avatar, stats}) => {
+    return (
+        <>
+        <div>
+            <div>
+                <img
+                    src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+                    alt="user avatar"
+                    class="avatar"
+                />
+                <p>Petra Marica</p>
+                <p>@pmarica</p>
+                <p>Salvador, Brasil</p>
+            </div>
 
+            <ul class="stats">
+              <li>
+                <span class="label">Followers</span>
+                <span class="quantity">1000</span>
+              </li>
+              <li>
+                <span class="label">Views</span>
+                <span class="quantity">2000</span>
+              </li>
+              <li>
+                <span class="label">Likes</span>
+                <span class="quantity">3000</span>
+              </li>
+            </ul>
+        </div>
+        </>
+    )
+}
 export const Product = ({name, price, children}) => {
   
   return (
